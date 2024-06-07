45d1df6430559c6c8a80046f66daed0bb48351b8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-07 23:36:53 +0300

    initial commit

---
+++ models/vehicle.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const vehicleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  size: { type: String, required: true },
+  isAvailable: { type: Boolean, required: true },
+})
+
+vehicleSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject_id.toString()
+
+    returnedObject._id
+    returnedObject.__v
+  }
+})
+
+module.exports = vehicleSchema

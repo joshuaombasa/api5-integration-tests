0513039c76937ffd66c8ce24a3baa99137fc378e
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-10 21:21:58 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const usersRouter = require('./controllers/users')
+const vehiclesRouter = require('./controllers/vehicles')
+
 
 
 
 const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(middleware.requestLOgger)
+
+app.use('/api/users', usersRouter)
+app.use('/api/vehicles', vehiclesRouter)
+
+app.use(middleware.unknownEndpointHandler)
+app.use(middleware.errorHandler)
 
 
 module.exports = app

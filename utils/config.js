11b5844f2a15989e41f25c462f698307aa7621c4
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-11 22:20:26 +0300

    initial commit

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-const TEST_MONGO_URI = process.env.NODE_ENV !== 'test' ? process.env.MONGO_URI : process.env.TEST_MONGO_URI
+const MONGO_URI = process.env.NODE_ENV !== 'test' ? process.env.MONGO_URI : process.env.TEST_MONGO_URI
 
-module.exports = { PORT, TEST_MONGO_URI }
+module.exports = { PORT, MONGO_URI }

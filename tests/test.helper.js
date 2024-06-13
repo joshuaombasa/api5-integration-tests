c5ebedee77430dcddfa45bb88e6b6fd74f339063
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-13 22:36:58 +0300

    add integration tests

---
+++ tests/test.helper.js
@@ -0,0 +1,40 @@
+const vehiclesData = [{
+  name: 'vehicle1',
+  price: '100',
+  size: 'small',
+  isAvailable: false
+},
+{
+  name: 'vehicle2',
+  price: '200',
+  size: 'small',
+  isAvailable: false
+},
+{
+  name: 'vehicle3',
+  price: '100',
+  size: 'small',
+  isAvailable: false
+},
+{
+  name: 'vehicle4',
+  price: '400',
+  size: 'small',
+  isAvailable: false
+},
+{
+  name: 'vehicle5',
+  price: '500',
+  size: 'small',
+  isAvailable: false
+},
+{
+  name: 'vehicle6',
+  price: '600',
+  size: 'small',
+  isAvailable: false
+},]
+
+
+
+module.exports = { vehiclesData }

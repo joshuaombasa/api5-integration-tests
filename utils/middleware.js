4bb0e8d22de0854f5b63a4113aef3cdbd888bf81
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-16 20:57:57 +0300

    add integration tests

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,7 +1,7 @@
 
 const logger = require('./logger')
 
-const requestLOgger = (request, response,next) => {
+const requestLOgger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:', request.path)
   logger.info('Body:', request.body)
@@ -17,11 +17,11 @@ const errorHandler = (request, response) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
-    response.status(400).send({ errors: { message: error.message } })
+    return response.status(400).send({ errors: { message: error.message } })
   }
 
   if (error.name === 'ValidationError') {
-    response.status(400).send({ errors: { message: error.message } })
+    return response.status(400).send({ errors: { message: error.message } })
   }
 
   response.status(400).send({ errors: { message: 'Something went wrong' } })

4100d93b3f3250d1a93f585e63d63796589150ab
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-06-16 21:32:28 +0300

    add integration tests

--- app.js
+++ app.js
@@ -24,9 +24,7 @@ app.use(middleware.requestLOgger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/vehicles', vehiclesRouter)
-// app.use('/api/vehicles', (request,response) => {
-//      response.send({})
-// })
+
 
 app.use(middleware.unknownEndpointHandler)
 app.use(middleware.errorHandler)
